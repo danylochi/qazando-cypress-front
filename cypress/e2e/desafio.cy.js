9803392b3b09fbbac06f27ffbb43e3578e68fa1f
danylochi <danylochi@gmail.com>
2024-08-27 22:41:41 -0300

    aula 2024-08-26

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -1,61 +1,85 @@
 describe('Cadastro de usuário', () => {
    
-    it('Validar campo nome vazio', () => {
+    it('Validar campo span de email vazio', () => {
     cy.visit('/'+'login')
-    cy.get('#btnLogin').click()
-    cy.get('.invalid_input').contains('E-mail inválido')   
+    
+    cy.get('.account_form > h3')
+        .should('be.visible')
+    
+    cy.get('#btnLogin')
+        .click()
+    
+    cy.get('.invalid_input')
+        .should('contain.text', 'E-mail inválido')
+        .and('be.visible');
+      
 
     })
 
-    it('Validar campo e-mail vazio', () => {
+    it('Validar o campo senha invalida vazio', () => {
+        // Visita a página de login
         cy.visit('/'+'login')
         
+        // Digita um e-mail válido no campo de usuário
         cy.get('#user').type('12345@gmail.com')
 
-        /*cy.get('#password')
-            .type()*/
-
+        
+        // Clica no botão de login
         cy.get('#btnLogin').click()
-
+        
+        // Verifica se a mensagem de erro "Senha inválida" é exibida
         cy.get('.invalid_input')
-            .contains('Senha inválida')
-
-
+            .should('contain.text','Senha inválida')
+            .and('be.visible')
 
     })
 
-    it ('Validar campo senha inválido', () => {
+    it ('Validar span campo senha tipo inválido', () => {
         cy.visit('/'+'login')
         
-        cy.get('#user').type('12345@gmail.com')
+        cy.get('#user')
+            .should('be.visible')
+            .type('12345@gmail.com')
 
         cy.get('#password')
+            .should('be.visible')
             .type('1')
 
-        cy.get('#btnLogin').click()
+        cy.get('#btnLogin')
+            .click()
         
         cy.get('.invalid_input')
-            .contains('Senha inválida')
+            .should('have.text','Senha inválida.')
 
 
     })
     
 
-    it ('Cadastro realizado com sucesso', () => {
+    it ('Login realizado com sucesso', () => {
         cy.visit('/'+'login')   
 
-        cy.get('#user').type('12345@gmail.com')
+        cy.get('#user')
+            .should('be.visible')
+            .type('12345@gmail.com')
 
         cy.get('#password')
-        .type('123456@A')
+            .should('be.visible')
+            .type('123456@A')
 
-        cy.get('#btnLogin').click()
+        cy.get('#btnLogin')
+            .should('be.visible')
+            .click()
        
-        cy.clock()
-        cy.get('#swal2-html-container').contains('12345@gmail.com')
-        cy.tick(8000)
+
         
-        cy.get('.swal2-confirm').click()
+        cy.get('#swal2-html-container')
+            .should('be.visible')
+            .should('contain.text','Olá, 12345@gmail.com')
+
+        
+        cy.get('.swal2-confirm')
+            .should('be.visible')
+            .click() // Confirma a ação
 
     })
 })
